3af601e58a40cb08b928827ae84053817125a8d8
Yurii Krasniuk <y.krasniuk.rt01@kpi.ua>
2022-03-26 20:25:47 +0200

    done! complex has ready to exploitation

--- Complex/test.cpp
+++ Complex/test.cpp
@@ -5,7 +5,21 @@ using namespace std;
 
 int main()
 {
-	complex a;
+	complex a(3, 4);
+	complex c;
+	cout << "Enter a complex number (q to quit):\n";
+	while (cin >> c)
+	{
+		cout << "c is " << c << endl;
+		cout << "complex conjucate is " << -c << endl;
+		cout << "a is " << a << endl;
+		cout << "a + c is " << a + c << endl;
+		cout << "a - c is " << a - c << endl;
+		cout << "a * c is " << a * c << endl;
+		cout << "2 * c is " << 2 * c << endl;
+		cout << "Enter a complex number (q to quit):\n";
+	}
+	cout << "Done!\n";
 
 	return 0;
 }
